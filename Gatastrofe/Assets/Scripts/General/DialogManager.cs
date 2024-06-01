9b9b2bebc1a2990b4479284c900d104370305d14
BL4Z3-Sebas <158209851+BL4Z3-Sebas@users.noreply.github.com>
2024-06-01 16:35:47 -0500

    Gestor de Dialogo Finalizado
    -Organización del DialogManager.txt
    -Ultimo cambio del sprict DialogManager
    -Correcion de la lectura de una lista a un Diccionario

--- Gatastrofe/Assets/Scripts/General/DialogManager.cs
+++ Gatastrofe/Assets/Scripts/General/DialogManager.cs
@@ -7,21 +7,90 @@ public class DialogManager : MonoBehaviour
 {
     private const string ruta = "Assets\\Files\\DialogManager.txt"; // Constante que guarda la ruta de donde se leen los dialogos del juego
     readonly Dictionary<string, string> DialogDictionary = new(); // Diccionario que almacena los dialogos del juego por <Codigo,Dialogo>
-  
+     private bool isPlayerInRange=false;
+    
+    
+    
+    [SerializeField] private static string DialogLine;
+    [SerializeField] private GameObject DialoguePanel;
+    [SerializeField] private TMP_Text DialogueText;
+    [SerializeField] private int CantDialog;
+    private int counter=1;
+    
+    // Update is called once per frame
 
     // Metodo para que no se destruya el objeto una vez se cambia de escena
-    private void Awake()
-    {
+    void Start() {
+
         DontDestroyOnLoad(this);
         LeerArchivo();
     }
-    // Start is called before the first frame update
-   
-    // Update is called once per frame
     void Update()
     {
+        if (isPlayerInRange)
+        {
+            
+                StartDialogue();
+            
+        }
+    }
+
+    
+      private void StartDialogue()
+    {
+        
+        DialoguePanel.SetActive(true);
+      
+        
+        ShowDialogues();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+
+            this.isPlayerInRange = true;
+            Debug.Log("puede interacturar");
+        }
+
 
     }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            this.isPlayerInRange = false;
+            Debug.Log("no puede interacturar");
+        }
+    }
+
+private void ShowLine(string line)
+    {
+        DialogueText.text = line;
+    }
+
+    private void ShowDialogues()
+    {
+         
+
+
+        if(Input.GetButtonDown("Fire1")){
+        
+            ShowLine(ObtenerDialogo(DialogLine+counter));
+            // You can add a delay here if you want to simulate waiting
+        counter++;
+        }
+        if(counter>CantDialog){
+              
+             DialoguePanel.SetActive(false);
+             counter=1;
+        }
+      
+    }
+
+
+
 
     // Lee el archivo de DialogManager para cargar la lista de dialogos en el diccionario
     public void LeerArchivo()
